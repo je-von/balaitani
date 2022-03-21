24497e1f597b4a46186d8185456e0e5f7ce0aae9
jevon <jevon.levin@gamil.com>
2022-03-21 16:12:54 +0700

    add user address column

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -14,26 +15,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Factory::create();
+
         DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@mail.com',
             'password' => bcrypt('admin123'),
+            'address' => $faker->address,
             'role' => 'admin'
         ]);
         DB::table('users')->insert([
             'name' => 'Jevon',
             'email' => 'jevon@mail.com',
             'password' => bcrypt('jevon123'),
+            'address' => $faker->address,
         ]);
         DB::table('users')->insert([
             'name' => 'Louis',
             'email' => 'louis@mail.com',
             'password' => bcrypt('louis123'),
+            'address' => $faker->address,
         ]);
         DB::table('users')->insert([
             'name' => 'Beni',
             'email' => 'beni@mail.com',
             'password' => bcrypt('beni123'),
+            'address' => $faker->address,
         ]);
     }
 }
